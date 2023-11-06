67c89fc235ce5636f739574582f32910d5769ff4
Marcinos <60058865+marcinosanalyses@users.noreply.github.com>
2023-11-06 21:41:39 +0100

    Update and rename vast14.sh to vast6.sh

--- vast6.sh
+++ vast6.sh
@@ -48,22 +48,6 @@ sleep 1
 sudo nohup ./xengpuminer -d4 > xengpuminer-4.log 2>&1 &
 sleep 1
 sudo nohup ./xengpuminer -d5 > xengpuminer-5.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d6 > xengpuminer-6.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d7 > xengpuminer-7.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d8 > xengpuminer-8.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d9 > xengpuminer-9.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d10 > xengpuminer-10.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d11 > xengpuminer-11.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d12 > xengpuminer-12.log 2>&1 &
-sleep 1
-sudo nohup ./xengpuminer -d13 > xengpuminer-13.log 2>&1 &
 
 echo "-------------------------"
 echo "    MINING  XENBLOCKS    "
